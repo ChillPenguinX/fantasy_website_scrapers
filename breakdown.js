e75e08ab5ba9e0980f8e2358e32091f0fed9c28a
Andrew Brasuell <abraz@BrazBookPro.local>
2013-04-16 21:08:05 -0400

    first commit

---
+++ breakdown.js
@@ -0,0 +1,166 @@
+var js = document.createElement("script");
+js.type = "text/javascript";
+js.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js';
+document.body.appendChild(js);
+function bd() {
+
+  var hist = {
+    "Chicago Dawgs": {W: 19, L: 2, T: 1, WH: 17, LH: 4, TH: 1, WP: 17, LP: 3, TP: 2, p: "............. "},
+    "Assault Rifle Hunters": {W: 16, L: 5, T: 1, WH: 11, LH: 10, TH: 1, WP: 16, LP: 1, TP: 5, p: "..... "},
+    "Colt .45s": {W: 13, L: 8, T: 1, WH: 14, LH: 8, TH: 0, WP: 7, LP: 12, TP: 3, p: "...................... "},
+    "Irish Guinness07": {W: 10, L: 10, T: 2, WH: 13, LH: 8, TH: 1, WP: 7, LP: 14, TP: 1, p: "........... "},
+    "I Punt Cats": {W: 10, L: 10, T: 2, WH: 18, LH: 2, TH: 2, WP: 10, LP: 12, TP: 0, p: "................... "},
+    "Tropical Storm Braz": {W: 10, L: 12, T: 0, WH: 9, LH: 12, TH: 1, WP: 2, LP: 16, TP: 4, p: "...... "},
+    "The Brewsers": {W: 9, L: 12, T: 1, WH: 7, LH: 13, TH: 2, WP: 9, LP: 11, TP: 2, p: "............... "},
+    "Don't Mess with Texas": {W: 8, L: 11, T: 3, WH: 4, LH: 16, TH: 2, WP: 13, LP: 7, TP: 2, p: "... "},
+    "Football Tailgater": {W: 9, L: 13, T: 0, WH: 11, LH: 11, TH: 0, WP: 8, LP: 12, TP: 2, p: ".......... "},
+    "Irish Dawgs": {W: 7, L: 13, T: 2, WH: 5, LH: 14, TH: 3, WP: 12, LP: 8, TP: 2, p: ".................. "},
+    "The Emperor's Club": {W: 6, L: 13, T: 3, WH: 6, LH: 15, TH: 1, WP: 10, LP: 9, TP: 3, p: "....... "},
+    "Wilpon Still Sucks": {W: 6, L: 14, T: 2, WH: 9, LH: 11, TH: 2, WP: 6, LP: 12, TP: 4, p: ".......... "}
+  };
+
+
+  var cats = ['Runs', 'Homeruns', 'Runs Batted In', 'Stolen Bases', 'On-Base Percentage', 'Slugging Percentage', 
+              'Quality Starts', 'Wins', 'Saves', 'Earned Run Average', 'Walks and Hits per Inning Pitched', 'Strikeouts per Nine'];
+  var periods = ['............................................. ','...................................... ','............................... ',
+                 '.................................. ','....................... ','....................... '
+                 ,'................................. ', '.............................................. ',
+                 '............................................ ', '....................... ','..... ','......................... '];
+  var stats = {};
+  var teams = [];
+  var highs = {};
+  var row, statObj, teamName, histObj;
+  $('.linescoreTeamRow').each(function() {
+    row = $(this);
+    teamName = row.find('.teamName a').text();
+    teams.push(teamName);
+    statObj = stats[teamName] = {};
+    statObj['W']=statObj['L']=statObj['T']=statObj['WH']=statObj['LH']=statObj['TH']=statObj['WP']=statObj['LP']=statObj['TP']=0;
+    row.find('.precise').each(function(index) {
+      statObj[cats[index]] = parseFloat($(this).text());
+    });
+  });
+  var teamAWinsH, teamBWinsH, teamAWinsP, teamBWinsP, i, j, k, teamAVal, teamBVal;
+  teamA = stats[teams[0]]
+  for (k=0; k<cats.length; k++) {
+    highs[cats[k]] = {teams: [teams[0]], val: teamA[cats[k]]};
+  }
+  for (i=0; i<teams.length; i++) {
+    for (j=i+1; j<teams.length; j++) {
+      teamA = stats[teams[i]];
+      teamB = stats[teams[j]];
+      teamAWinsH = teamBWinsH = teamAWinsP = teamBWinsP = 0;
+      for (k=0; k<cats.length; k++) {
+        teamAVal = teamA[cats[k]];
+        teamBVal = teamB[cats[k]];
+        if (teamAVal > teamBVal) {
+          if (k < 6) {
+            teamAWinsH++;
+          } else if (k == 10 || k == 9) {
+            teamBWinsP++;
+          } else {
+            teamAWinsP++;
+          }
+        } else if (teamAVal < teamBVal) {
+          if (k < 6) {
+            teamBWinsH++;
+          } else if (k == 10 || k == 9) {
+            teamAWinsP++;
+          } else {
+            teamBWinsP++;
+          }
+        }
+        if (j == i+1) {
+          if ((highs[cats[k]]['val'] < teamBVal && k != 9 && k != 10) ||
+              (highs[cats[k]]['val'] > teamBVal && (k == 9 || k == 10))) {
+            highs[cats[k]]['teams'] = [teams[j]];
+            highs[cats[k]]['val'] = teamBVal;
+          } else if (highs[cats[k]]['val'] == teamBVal) {
+            highs[cats[k]]['teams'].push(teams[j]);
+          }
+        }
+      }
+      if ((teamAWinsH + teamAWinsP) > (teamBWinsH + teamBWinsP)) {
+        teamA['W']++;
+        teamB['L']++;
+      } else if ((teamAWinsH + teamAWinsP) < (teamBWinsH + teamBWinsP)) {
+        teamA['L']++;
+        teamB['W']++;
+      } else {
+        teamA['T']++;
+        teamB['T']++;
+      }
+      if (teamAWinsH > teamBWinsH) {
+        teamA['WH']++;
+        teamB['LH']++;
+      } else if (teamAWinsH < teamBWinsH) {
+        teamA['LH']++;
+        teamB['WH']++;
+      } else {
+        teamA['TH']++;
+        teamB['TH']++;
+      }
+      if (teamAWinsP > teamBWinsP) {
+        teamA['WP']++;
+        teamB['LP']++;
+      } else if (teamAWinsP < teamBWinsP) {
+        teamA['LP']++;
+        teamB['WP']++;
+      } else {
+        teamA['TP']++;
+        teamB['TP']++;
+      }
+    }
+  }
+  teams.sort(function(a,b){
+    teamA = stats[a];
+    teamB = stats[b];
+    return ((teamB['W'] + teamB['T']/2)/11) - ((teamA['W'] + teamA['T']/2)/11);
+  });
+  console.log('[b][u]Week Breakdowns (Combined, Hitting, Pitching)[/u][/b]')
+  for (i=0; i<teams.length; i++) {
+    statObj = stats[teams[i]];
+    histObj = hist[teams[i]]
+    histObj['W']  += statObj['W'];
+    histObj['L']  += statObj['L'];
+    histObj['T']  += statObj['T'];
+    histObj['WH'] += statObj['WH'];
+    histObj['LH'] += statObj['LH'];
+    histObj['TH'] += statObj['TH'];
+    histObj['WP'] += statObj['WP'];
+    histObj['LP'] += statObj['LP'];
+    histObj['TP'] += statObj['TP'];
+    console.log(teams[i] + histObj['p'] + '[b]' + statObj['W'] + '-' + statObj['L'] + '-' + statObj['T'] + '[/b]' + 
+                           ', ' + statObj['WH'] + '-' + statObj['LH'] + '-' + statObj['TH'] + 
+                           ', ' + statObj['WP'] + '-' + statObj['LP'] + '-' + statObj['TP']);
+  }
+  var totalGames = histObj['W'] + histObj['L'] + histObj['T'];
+  teams.sort(function(a,b){
+    teamA = hist[a];
+    teamB = hist[b];
+    return ((teamB['W'] + teamB['T']/2)/totalGames) - ((teamA['W'] + teamA['T']/2)/totalGames);
+  });
+  console.log('\n[b][u]Season Breakdowns (Combined, Hitting, Pitching)[/u][/b]')
+  for (i=0; i<teams.length; i++) {
+    histObj = hist[teams[i]]
+    console.log(teams[i] + histObj['p'] + '[b]' + histObj['W'] + '-' + histObj['L'] + '-' + histObj['T'] + '[/b]' +
+                           ', ' + histObj['WH'] + '-' + histObj['LH'] + '-' + histObj['TH'] +
+                           ', ' + histObj['WP'] + '-' + histObj['LP'] + '-' + histObj['TP']);
+  }
+  console.log('\n[b][u]Week Highs:[/u][/b]');
+  for (i=0; i<cats.length; i++) {
+    if (i == 6) {console.log(' ');}
+    console.log(cats[i] + periods[i] + highs[cats[i]]['val'] + ' - ' + highs[cats[i]]['teams'].join('; '));
+  }
+
+  console.log('\n\nvar hist = {')
+  for (i=0; i<teams.length; i++) {
+    histObj = hist[teams[i]];
+    console.log('\t\t"' + teams[i] + '": {W: ' + histObj['W'] + ', L: ' + histObj['L'] + ', T: ' + 
+      histObj['T'] + ', WH: ' + histObj['WH'] + ', LH: ' + histObj['LH'] + ', TH: ' + 
+      histObj['TH'] + ', WP: ' + histObj['WP'] + ', LP: ' + histObj['LP'] + ', TP: ' + 
+      histObj['TP'] + ', p: "' + histObj['p'] + '"}' + (i == teams.length-1 ? '' : ','));
+  }
+  console.log('\t};');
+}
+//type bd()
---
+++ fantasy_baseball.js
@@ -0,0 +1,78 @@
+//copy/paste this block into the Chrome console on the draft recap page and then run sort()
+
+var js = document.createElement("script");
+js.type = "text/javascript";
+js.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js';
+document.body.appendChild(js);
+var sort = function() {
+  var i, row;
+  var rows = $('tr.tableBody');
+  var body = $('body');
+  var table = $('<table>');
+  for (i=1; i <= rows.size(); i++) {
+    rows.each(function() {
+      row = $(this);
+      if (parseInt(row.find('td:first').text()) == i) {
+        table.append(row.append($('<td>').text(row.parent().find('.tableHead a').attr('title'))));
+        return false;
+      }
+    });
+  }
+  body.html(table);
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+var js = document.createElement("script");
+js.type = "text/javascript";
+js.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js';
+document.body.appendChild(js);
+function hitting(players) 
+{
+  var numPlayers = players.length;
+  var price = 0, r = 0, hr = 0, rbi = 0, sb = 0, obps = [], slgs = [], abs = [], ab = 0, i, tds, idx, p;
+  for (i=0; i < numPlayers; i++)
+  {
+    $('tr').each(function()
+    {
+      tds = $(this).find('td');
+      idx = $.inArray($(tds[0]).text(), players);
+      if (idx != -1) 
+      {
+        p = Math.max(parseInt($(tds[3]).text()), 1);
+        console.log('found ' + $(tds[0]).text() + ' - $' + p);
+        price += p;
+        abs.push(parseInt($(tds[4]).text()));
+        ab += parseInt($(tds[4]).text());
+        hr += parseInt($(tds[5]).text());
+        rbi += parseInt($(tds[6]).text());
+        sb += parseInt($(tds[7]).text());
+        r += parseInt($(tds[8]).text());
+        obps.push(parseFloat($(tds[10]).text()));
+        slgs.push(parseFloat($(tds[9]).text()));
+        players[idx] = 'xxxxxxxx';
+        return false;
+      }
+    });
+  }
+  var slg = 0, obp = 0;
+  for (i=0; i<abs.length; i++) 
+  {
+    slg += slgs[i] * abs[i];
+    obp += obps[i] * abs[i];
+  }
+  slg = slg / ab;
+  obp = obp / ab;
+  console.log("$" + price + "\nR: " + r + " (878)\nHR: " + hr + " (254)\nRBI: " + rbi + " (916)\nSB: " + sb + " (174)\nOBP: " + obp + 
+    "\nSLG: " + slg, "\nOPS: " + (obp + slg) + " (.8325)");
+}
+
+hitting(['Carlos Santana', 'Edwin Encarnacion', 'Dustin Pedroia', 'Josh Rutledge', 'Miguel Cabrera', 'Mike Trout', 
+  'Jay Bruce', 'Matt Kemp', 'Adrian Gonzalez', 'Chris Davis'])
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
