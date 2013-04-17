f2b35cc46eeafa8e4b121ad9368229feb2d3224b
Andrew Brasuell <abraz@BrazBookPro.local>
2013-04-16 22:13:46 -0400

    make breakdown.js generic and have hlb-breakdown as an example

--- breakdown-hlb.js
+++ breakdown-hlb.js
@@ -5,34 +5,36 @@ document.body.appendChild(js);
 function bd() {
 
 
-
   //put your team names here. this assumes 12 team, but you can adjust add more or remove some if needed
   //the periods are for display purposes. You can remove them, make them different lengths, and replace them.
   //each time you run this, a new version of this object will be printed last. The idea is for you to copy that
   //and paste it here to keep a running total for breakdowns. The team names *will* need to match text on the site.
   var hist = {
-    "": {W: 0, L: 0, T: 0, WH: 0, LH: 0, TH: 0, WP: 0, LP: 0, TP: 0, p: ".. "},
-    "": {W: 0, L: 0, T: 0, WH: 0, LH: 0, TH: 0, WP: 0, LP: 0, TP: 0, p: ".. "},
-    "": {W: 0, L: 0, T: 0, WH: 0, LH: 0, TH: 0, WP: 0, LP: 0, TP: 0, p: ".. "},
-    "": {W: 0, L: 0, T: 0, WH: 0, LH: 0, TH: 0, WP: 0, LP: 0, TP: 0, p: ".. "},
-    "": {W: 0, L: 0, T: 0, WH: 0, LH: 0, TH: 0, WP: 0, LP: 0, TP: 0, p: ".. "},
-    "": {W: 0, L: 0, T: 0, WH: 0, LH: 0, TH: 0, WP: 0, LP: 0, TP: 0, p: ".. "},
-    "": {W: 0, L: 0, T: 0, WH: 0, LH: 0, TH: 0, WP: 0, LP: 0, TP: 0, p: ".. "},
-    "": {W: 0, L: 0, T: 0, WH: 0, LH: 0, TH: 0, WP: 0, LP: 0, TP: 0, p: ".. "},
-    "": {W: 0, L: 0, T: 0, WH: 0, LH: 0, TH: 0, WP: 0, LP: 0, TP: 0, p: ".. "},
-    "": {W: 0, L: 0, T: 0, WH: 0, LH: 0, TH: 0, WP: 0, LP: 0, TP: 0, p: ".. "},
-    "": {W: 0, L: 0, T: 0, WH: 0, LH: 0, TH: 0, WP: 0, LP: 0, TP: 0, p: ".. "},
-    "": {W: 0, L: 0, T: 0, WH: 0, LH: 0, TH: 0, WP: 0, LP: 0, TP: 0, p: ".. "}
+    "Chicago Dawgs": {W: 19, L: 2, T: 1, WH: 17, LH: 4, TH: 1, WP: 17, LP: 3, TP: 2, p: "............. "},
+    "Assault Rifle Hunters": {W: 16, L: 5, T: 1, WH: 11, LH: 10, TH: 1, WP: 16, LP: 1, TP: 5, p: "..... "},
+    "Colt .45s": {W: 13, L: 8, T: 1, WH: 14, LH: 8, TH: 0, WP: 7, LP: 12, TP: 3, p: "...................... "},
+    "Irish Guinness07": {W: 10, L: 10, T: 2, WH: 13, LH: 8, TH: 1, WP: 7, LP: 14, TP: 1, p: "........... "},
+    "I Punt Cats": {W: 10, L: 10, T: 2, WH: 18, LH: 2, TH: 2, WP: 10, LP: 12, TP: 0, p: "................... "},
+    "Tropical Storm Braz": {W: 10, L: 12, T: 0, WH: 9, LH: 12, TH: 1, WP: 2, LP: 16, TP: 4, p: "...... "},
+    "The Brewsers": {W: 9, L: 12, T: 1, WH: 7, LH: 13, TH: 2, WP: 9, LP: 11, TP: 2, p: "............... "},
+    "Don't Mess with Texas": {W: 8, L: 11, T: 3, WH: 4, LH: 16, TH: 2, WP: 13, LP: 7, TP: 2, p: "... "},
+    "Football Tailgater": {W: 9, L: 13, T: 0, WH: 11, LH: 11, TH: 0, WP: 8, LP: 12, TP: 2, p: ".......... "},
+    "Irish Dawgs": {W: 7, L: 13, T: 2, WH: 5, LH: 14, TH: 3, WP: 12, LP: 8, TP: 2, p: ".................. "},
+    "The Emperor's Club": {W: 6, L: 13, T: 3, WH: 6, LH: 15, TH: 1, WP: 10, LP: 9, TP: 3, p: "....... "},
+    "Wilpon Still Sucks": {W: 6, L: 14, T: 2, WH: 9, LH: 11, TH: 2, WP: 6, LP: 12, TP: 4, p: ".......... "}
   };
   //list your categories here in the order they appear on the scoreboard. They do not need to match the text on the site. 
-  var cats = ['R', 'HR', 'RBI', 'SB', 'AVG', 
-              'K', 'W', 'SV', 'ERA', 'WHIP'];
+  var cats = ['Runs', 'Homeruns', 'Runs Batted In', 'Stolen Bases', 'On-Base Percentage', 'Slugging Percentage', 
+              'Quality Starts', 'Wins', 'Saves', 'Earned Run Average', 'Walks and Hits per Inning Pitched', 'Strikeouts per Nine'];
   //list any categories where it's better to have a lower number here, in any order. They need to match the text in cats. 
-  var neg_cats = ['ERA', 'WHIP'];
+  var neg_cats = ['Earned Run Average', 'Walks and Hits per Inning Pitched'];
   //how many hitting categories do you have?
-  var num_hitting_cats = 5;
+  var num_hitting_cats = 6;
   //for display purposes
-  var periods = ['.. ', '.. ', '.. ', '.. ', '.. ', '.. ', '.. ', '.. ', '.. ', '.. '];
+  var periods = ['............................................. ','...................................... ','............................... ',
+                 '.................................. ','....................... ','....................... '
+                 ,'................................. ', '.............................................. ',
+                 '............................................ ', '....................... ','..... ','......................... '];
 
 
 
---
+++ histObj.js
@@ -0,0 +1,14 @@
+var hist = {
+    "Chicago Dawgs": {W: 0, L: 0, T: 0, WH: 0, LH: 0, TH: 0, WP: 0, LP: 0, TP: 0, p: "............. "},
+    "Assault Rifle Hunters": {W: 0, L: 0, T: 0, WH: 0, LH: 0, TH: 0, WP: 0, LP: 0, TP: 0, p: "..... "},
+    "Colt .45s": {W: 0, L: 0, T: 0, WH: 0, LH: 0, TH: 0, WP: 0, LP: 0, TP: 0, p: "...................... "},
+    "I Punt Cats": {W: 0, L: 0, T: 0, WH: 0, LH: 0, TH: 0, WP: 0, LP: 0, TP: 0, p: "................... "},
+    "Irish Dawgs": {W: 0, L: 0, T: 0, WH: 0, LH: 0, TH: 0, WP: 0, LP: 0, TP: 0, p: ".................. "},
+    "Irish Guinness07": {W: 0, L: 0, T: 0, WH: 0, LH: 0, TH: 0, WP: 0, LP: 0, TP: 0, p: "........... "},
+    "Tropical Storm Braz": {W: 0, L: 0, T: 0, WH: 0, LH: 0, TH: 0, WP: 0, LP: 0, TP: 0, p: "...... "},
+    "The Emperor's Club": {W: 0, L: 0, T: 0, WH: 0, LH: 0, TH: 0, WP: 0, LP: 0, TP: 0, p: "....... "},
+    "Don't Mess with Texas": {W: 0, L: 0, T: 0, WH: 0, LH: 0, TH: 0, WP: 0, LP: 0, TP: 0, p: "... "},
+    "Wilpon Still Sucks": {W: 0, L: 0, T: 0, WH: 0, LH: 0, TH: 0, WP: 0, LP: 0, TP: 0, p: ".......... "},
+    "Football Tailgater": {W: 0, L: 0, T: 0, WH: 0, LH: 0, TH: 0, WP: 0, LP: 0, TP: 0, p: ".......... "},
+    "The Brewsers": {W: 0, L: 0, T: 0, WH: 0, LH: 0, TH: 0, WP: 0, LP: 0, TP: 0, p: "............... "}
+  };
