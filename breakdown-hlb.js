48e910aab823a2ba2489814835381fdd700615b3
Andrew Brasuell <andrew.brasuell@tapjoy.com>
2017-09-05 09:39:03 -0400

    week 21

--- breakdown-hlb.js
+++ breakdown-hlb.js
@@ -10,14 +10,14 @@ function bd() {
   //each time you run this, a new version of this object will be printed last. The idea is for you to copy that
   //and paste it here to keep a running total for breakdowns. The team names *will* need to match text on the site.
 var hist = {
-		"Prospect Hoarders": {Wi: 87, L: 42, T: 11, WH: 64, LH: 54, TH: 22, WP: 96, LP: 32, TP: 12, p: "................ "},
-		"Colt .45s": {Wi: 82, L: 45, T: 13, WH: 81, LH: 42, TH: 17, WP: 65, LP: 57, TP: 18, p: "............................. "},
-		"I Punt Cats": {Wi: 76, L: 49, T: 15, WH: 70, LH: 51, TH: 19, WP: 91, LP: 37, TP: 12, p: ".......................... "},
-		"The Mike Shitty All-Stars": {Wi: 75, L: 51, T: 14, WH: 72, LH: 48, TH: 20, WP: 61, LP: 68, TP: 11, p: "...... "},
-		"Tropical Storm Braz": {Wi: 68, L: 59, T: 13, WH: 53, LH: 71, TH: 16, WP: 68, LP: 56, TP: 16, p: "............. "},
-		"Irish Guinness07": {Wi: 65, L: 58, T: 17, WH: 71, LH: 48, TH: 21, WP: 63, LP: 61, TP: 16, p: "................. "},
-		"Don't Mess with Texas": {Wi: 25, L: 98, T: 17, WH: 26, LH: 96, TH: 18, WP: 33, LP: 94, TP: 13, p: ".......... "},
-		"Irish Dawgs": {Wi: 25, L: 101, T: 14, WH: 45, LH: 72, TH: 23, WP: 29, LP: 101, TP: 10, p: "........................ "}
+		"Prospect Hoarders": {Wi: 94, L: 42, T: 11, WH: 71, LH: 54, TH: 22, WP: 100, LP: 32, TP: 15, p: "................ "},
+		"Colt .45s": {Wi: 86, L: 48, T: 13, WH: 86, LH: 44, TH: 17, WP: 67, LP: 61, TP: 19, p: "............................. "},
+		"The Mike Shitty All-Stars": {Wi: 80, L: 53, T: 14, WH: 76, LH: 51, TH: 20, WP: 66, LP: 69, TP: 12, p: "...... "},
+		"I Punt Cats": {Wi: 79, L: 53, T: 15, WH: 72, LH: 56, TH: 19, WP: 96, LP: 38, TP: 13, p: ".......................... "},
+		"Tropical Storm Braz": {Wi: 74, L: 60, T: 13, WH: 59, LH: 72, TH: 16, WP: 73, LP: 57, TP: 17, p: "............. "},
+		"Irish Guinness07": {Wi: 67, L: 63, T: 17, WH: 74, LH: 52, TH: 21, WP: 65, LP: 65, TP: 17, p: "................. "},
+		"Don't Mess with Texas": {Wi: 26, L: 104, T: 17, WH: 27, LH: 102, TH: 18, WP: 34, LP: 100, TP: 13, p: ".......... "},
+		"Irish Dawgs": {Wi: 25, L: 108, T: 14, WH: 45, LH: 79, TH: 23, WP: 29, LP: 108, TP: 10, p: "........................ "}
 	};
 
   //list your categories here in the order they appear on the scoreboard. They do not need to match the text on the site.
